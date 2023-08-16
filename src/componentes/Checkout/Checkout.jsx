92a24c437105df43464e1ab5fa389c8374c7056e
silvia <silviavergara.14sa@gmail.com>
2023-08-16 01:07:05 -0400

    Checkout con descuento de stock

--- src/componentes/Checkout/Checkout.jsx
+++ src/componentes/Checkout/Checkout.jsx
@@ -1,125 +1,141 @@
-import { useState, useContext } from "react"
-import { CarritoContext } from "../../context/CarritoContext"
-import { db } from "../../services/config"
-import { collectio, addDoc, collection } from "firebase/firestore"
-import './Checkout.css';
-//Cuando se genere la orden de compra se limpia el carrito 
-const Checkout = () => {
-    const { carrito, vaciarCarrito, total } = useContext(CarritoContext);
+import { useState, useContext } from "react";
+import { CarritoContext } from '../../context/CarritoContext';
+import { db } from "../../services/config";
+import { collection, addDoc, updateDoc, doc, getDoc } from "firebase/firestore";
+import './Checkout.css'
 
-    //Trabajaremos con el formulario
+const Checkout = () => {
+    const { carrito, vaciarCarrito, cantidadTotal } = useContext(CarritoContext);
     const [nombre, setNombre] = useState("");
     const [apellido, setApellido] = useState("");
     const [telefono, setTelefono] = useState("");
-    //Ambos email es para confirmar que ambos correos estan escritos de manera igual sin diferencias 
     const [email, setEmail] = useState("");
-    const [emailConfim, setEmailConfirm] = useState("");
-    //El error nos avisa que algo fallo y lo capturamos para guardarlo en un estado 
+    const [emailConfirmacion, setEmailConfirmacion] = useState("");
     const [error, setError] = useState("");
-    const [ordenId, setOrdenId] = useState("");
-    //Y por ultimo con algun comentario que desee ingresar nuestro usuario
-    const [comentario, setComentario] = useState("");
+    const [orderId, setOrdenId] = useState("");
 
-    //Funciones y validaciones
 
-    const manejadorFormulario = (event) => {
-        event.preventDefault();
+
+    //Funciones y validaciones: 
+
+    const manejadorFormulario = (e) => {
+        e.preventDefault();
+
+        //Verificamos que los campos esten completos: 
+        if (!nombre || !apellido || !telefono || !email || !emailConfirmacion) {
+            setError("Todos los campos son obligatorios porfavor verifique que esten completos");
+            return;
+        }
+
+        //Validamos que los campos del email coincidan: 
+        if (email !== emailConfirmacion) {
+            setError("Los campos de email no coinciden porfavor verificar");
+            return;
+        }
+
+        //Creamos el objeto de la orden. 
+        const orden = {
+            items: carrito.map(producto => ({
+                id: producto.item.id,
+                nombre: producto.item.nombre,
+                cantidad: producto.cantidad
+            })),
+            total: cantidadTotal,
+            nombre,
+            apellido,
+            telefono,
+            email,
+            fecha: new Date()
+        };
+
+        //Vamos a modificar el código para que ejecute varias promesas en parelelo, por un lado quiero que actualice el stock de productos y por otro lado quiero que genere una orden de compra. Promise.All me permite todo esto. 
+
+        Promise.all(
+            orden.items.map(async (productoOrden) => {
+                const productoRef = doc(db, "inventario", productoOrden.id);
+                //Por cada producto en la coleecion "productos" obtengo una referencia.
+                const productoDoc = await getDoc(productoRef);
+                const stockActual = productoDoc.data().stock;
+                //Data me permite acceder a la información del documento. 
+
+                await updateDoc(productoRef, {
+                    stock: stockActual - productoOrden.cantidad
+                });
+                // subo la información actualizada. 
+            })
+        )
+            .then(() => {
+                //Guardamos la orden en la base de datos: 
+                addDoc(collection(db, "ordenes"), orden)
+                    .then((docRef) => {
+                        setOrdenId(docRef.id);
+                        vaciarCarrito();
+                    })
+                    .catch((error) => {
+                        console.log("Nos surgio un error en su compra porfavor vuelva a intentar", error);
+                        setError("Error al crear la orden, vuelva más tarde");
+                    })
+            })
+            .catch((error) => {
+                console.log("Error al actualizar el stock", error);
+                setError("Intente nuevamente");
+            })
+
     }
-    //Campos de texto esten completos menos el de comentario que es opcional
-
-    if (!nombre || !apellido || !telefono || !email || !emailConfim) {
-        setError("Por favor complete todos los campos requeridos");
-        return;
-    }
-
-    //Ahora verificamos que los campos de email esten exactamente iguales
-
-    if (email !== emailConfim) {
-        setError("Los email ingresados no estan iguales porfavor verifique que esten correctos");
-        return;
-    }
-    //Creamos el objeto de la orden
-
-    const orden = {
-        items: carrito.map(producto => ({
-            id: producto.item.id,
-            nombre: producto.item.nombre,
-            cantidad: producto.cantidad
-        })),
-        total: total,
-        nombre,
-        apellido,
-        telefono,
-        email,
-        fecha: new Date(),
-    }
-    //Guardamos la orden en la base de datos y le pasamos addDocs y la collection con su respectivo nombre de donde sacaremos los datos
-
-    addDoc(collection(db, "ordenes"), orden)
-        .then(docRef => {
-            setOrdenId(docRef.id);
-            vaciarCarrito();
-        })
-        .catch(error => {
-            console.log("Error al crear su orden", error);
-            setError("Se produjo un error al crear la orden de la compra, porfavor vuelva pronto")
-        })
-
 
     return (
         <div>
-            <h2>Verificamos tu compra</h2>
+            <h2>Checkout</h2>
             <form onSubmit={manejadorFormulario} className="formulario">
                 {carrito.map(producto => (
-                    <div key={producto.item.id}>
+                    <div key={producto.id}>
                         <p>
                             {producto.item.nombre} x {producto.cantidad}
                         </p>
-                        <p>Precio: $ {producto.item.precio}</p>
+                        <p>Precio $ {producto.item.precio} </p>
+                        <hr />
                     </div>
                 ))}
                 <hr />
-                <hr />
-//Aqui creamos lo visual
 
                 <div className="form-group">
-                    <label htmlFor="">Nombre </label>
+                    <label htmlFor="nombre"> Nombre </label>
                     <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
                 </div>
 
                 <div className="form-group">
-                    <label htmlFor="">Apellido</label>
+                    <label htmlFor=""> Apellido </label>
                     <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
                 </div>
 
                 <div className="form-group">
-                    <label htmlFor="">Telefono</label>
+                    <label htmlFor=""> Telefono </label>
                     <input type="text" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
                 </div>
 
                 <div className="form-group">
-                    <label htmlFor="">Email</label>
-                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <label htmlFor=""> Email </label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 </div>
 
                 <div className="form-group">
-                    <label htmlFor="">Confirmacion de email</label>
-                    <input type="text" value={emailConfim} onChange={(e) => setEmailConfirm(e.target.value)} />
+                    <label htmlFor=""> Email Confirmación </label>
+                    <input type="email" value={emailConfirmacion} onChange={(e) => setEmailConfirmacion(e.target.value)} />
                 </div>
 
-                <div className="form-group">
-                    <label htmlFor="">Deje un comentario opcional</label>
-                    <input type="text" value={comentario} onChange={(e) => setComentario(e.target.value)} />
-                </div>
-                {error && <p>{error}</p>}
-                <button type="submit">Finalizar compra</button>
+                {
+                    error && <p style={{ color: "red" }}> {error} </p>
+                }
 
+                <button type="submit"> Finalizar Compra </button>
             </form>
             {
-                ordenId && (
-                    <strong>¡Muchas gracias por su compra! Su numero de orden es {ordenId}</strong>
+                orderId && (
+                    <strong className="ordenId">Gracias por su compra el numero de su orden es {orderId}  para que realize el seguimiento correspondiente.</strong>
                 )
             }
+
+
         </div>
     )
 }
